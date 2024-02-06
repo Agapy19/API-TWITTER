5ee55163ddbde0fee66aa31786a6bca0e0f7f6f8
Agapy Mafo <mafoagapy697@gmail.com>
2024-02-06 13:01:00 +0200

    ajout des tweets

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express=  require ('express');
-const route = require ('./src/Routes/tweetRoute');
+const express = require('express');
+const route = require('./src/Routes/tweetRoute');
 const app = express();
 const port = 3002;
+app.use(express.json());
 app.use('/tweet', route);
-
 // Ecout du port
 app.listen(port, () => console.log('mon application est demarrer sur: http://localhost:$(port)'));
-   
--- src/Routes/tweetRoute.js
+++ src/Routes/tweetRoute.js
@@ -1,10 +1,33 @@
 
-const express=  require ('express');
-const Tweet = require ('/home/user-15-c1/Documents/PROJET/API TWITTER/src/Models/tweetModel.js')
+const express = require('express');
+const Tweets = require('/home/user-15-c1/Documents/PROJET/API TWITTER/src/Models/tweetModel.js')
 const router = express.Router()
 
+
+
 router.get("/", function (req, res) {
-    res.json(Tweet);
-  });
+  res.json(Tweets);
+
+});
+
+router.post("/", (req, res) => {
+  const { userid, id, title, body, url, thumbnailUrl, likes, repost } = req.body;
+  const tweet = {
+    "userid": userid,
+    "id": id,
+    "title": title,
+    "body": body,
+    "url": url,
+    "thumbnailUrl": thumbnailUrl,
+    "likes": likes,
+    "repost": repost
+  };
+  Tweets.push(tweet);
+  res.status(201).json(Tweets);
+
+});
+
+
+
 
 module.exports = router;
